55b25dd695b5f3ffde351072821c7f6e09c298cb
nerokome <nerooghenekome@gmail.com>
2025-08-06 17:17:11 +0100

    normal

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const cors = require('cors');
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://scah.club',
-  'https://scahnext.vercel.app'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
+app.use(cors({}));
 
 
 app.use(express.json());
