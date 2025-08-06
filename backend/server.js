6c046c5d3a0e60f80f1aef1e30199d35cfe157da
nerokome <nerooghenekome@gmail.com>
2025-08-06 15:47:56 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://scah.club', 'https://scah.club'],
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
