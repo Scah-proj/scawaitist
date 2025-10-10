ec65dde619b324449f5209c8c6f334f5e50011f8
nerokome <nerooghenekome@gmail.com>
2025-10-10 23:53:48 +0100

    tml

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 
 const allowedOrigins = [
-  'http://localhost:5500',
+  'http://127.0.0.1:5500',
   'https://waitlist-virid-eight.vercel.app/', 
   'https://scah.club',
   'http://scah.club',
